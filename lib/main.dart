786f02291c645c27cb652c159f1894f8bb2ec516
KimSihyun <rainy02190@gmail.com>
2024-07-22 22:13:54 +0900

    Feat:메인페이지

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:telo/screens/notification_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _PageSlideState();
+}
+
+class _PageSlideState extends State<MyApp> {
+  final PageController _pageController = PageController(initialPage: 0);
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double screenHeight = MediaQuery.of(context).size.height;
+    double indicatorPosition = screenHeight * 0.6;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            controller: _pageController,
+            children: [
+              Container(
+                color: Color(0xff93A98D),
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 90, 0, 0),
+                  child: Text(
+                    '임대인과\n임차인을 잇다\n\u{1f3e0}\nTELO',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      letterSpacing: 5,
+                      height: 1.8,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                color: Color(0xff93A98D),
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 90, 0, 0),
+                  child: Text(
+                    '현명한\n집 관리 플랜\nTELO로부터',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      letterSpacing: 5,
+                      height: 1.8,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: indicatorPosition,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: SmoothPageIndicator(
+                controller: _pageController,
+                count: 2,
+                effect: SwapEffect(
+                  activeDotColor: Colors.black,
+                  dotColor: Colors.black38
+                ),
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(onPressed: (){}, child: Text('구글 로그인', textAlign: TextAlign.center,)),
+              TextButton(onPressed: (){}, child: Text('카카오톡 로그인'))
+            ],
+          )
+        ],
+
+      ),
+    );
   }
 }
