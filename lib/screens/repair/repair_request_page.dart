c2e2595b34f11539e2d4b9d1e56ad6fbe4520a05
suminkim <angelsusum@gmail.com>
2024-07-24 02:18:42 +0900

    Feat: 수리 요청 페이지 수정

--- lib/screens/repair/repair_request_page.dart
+++ lib/screens/repair/repair_request_page.dart
@@ -12,10 +12,12 @@ class RepairRequestPage extends StatefulWidget {
 
 class _RepairRequestPageState extends State<RepairRequestPage> {
   final _formKey = GlobalKey<FormState>();
+  final ImagePicker _picker = ImagePicker();
 
   String _titleValue = "";
   String _descriptionValue = "";
   int _estimateValue = 0;
+  List<XFile> _pickedImages = [];
 
   @override
   Widget build(BuildContext context) {
@@ -31,88 +33,122 @@ class _RepairRequestPageState extends State<RepairRequestPage> {
                     Navigator.pop(context); // 뒤로가기
                   },
                 )),
-            body: Container(
-              padding: const EdgeInsets.all(30),
-              child: Form(
-                  key: _formKey,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("사진", style: TextStyle(fontSize: 15),),
-                        SizedBox(height: 10),
-                        IconButton(onPressed: () async {
-                          var picker = ImagePicker();
-                          var image = await picker.pickImage(source: ImageSource.gallery);
-                        }, icon: Icon(Icons.camera)),
-                        SizedBox(height: 10),
-                        Text("제목", style: TextStyle(fontSize: 15),),
-                        SizedBox(height: 10),
-                        TextFormField(
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return "필수 입력값입니다.";
-                            }
-                          },
-                          onSaved: (value) {
-                            setState(() {
-                              _titleValue = value!;
-                            });
-                          },
-                          maxLength: 15,
-                          decoration: const InputDecoration(
-                              hintText: "제목",
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Color(0xffD9D9D9),),
-                                borderRadius: BorderRadius.all(Radius.circular(10)),
-                              )
+            body: SingleChildScrollView(
+              child: Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(30),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "사진",
+                            style: TextStyle(fontSize: 15),
                           ),
-                        ),
-                        SizedBox(height: 10),
-                        Text("설명", style: TextStyle(fontSize: 15)),
-                        SizedBox(height: 10),
-                        TextFormField(
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return "필수 입력값입니다.";
-                            }
-                          },
-                          onSaved: (value) {
-                            setState(() {
-                              _descriptionValue = value!;
-                            });
-                          },
-                          maxLength: 200,
-                          maxLines: 6,
-                          decoration: const InputDecoration(
-                              hintText: "어떤 문제가 있는지 자세히 설명해 주세요.",
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(color: Color(0xffD9D9D9),),
-                                borderRadius: BorderRadius.all(Radius.circular(10)),
-                              )),
-                        ),
-                        SizedBox(height: 10),
-                        Text("예상 청구 금액 (선택 사항)", style: TextStyle(fontSize: 15)),
-                        SizedBox(height: 10),
-                        TextFormField(
-                          onSaved: (value) {
-                            setState(() {
-                              if (value != null && value.isNotEmpty) {
-                                _estimateValue = int.parse(value);
-                              } else {
-                                _estimateValue = 0;
+                          SizedBox(height: 10),
+                          // 사진 가져오기
+                          ElevatedButton(
+                            onPressed: () async {
+                              final List<XFile>? images =
+                                  await _picker.pickMultiImage();
+                              if (images != null) {
+                                setState(() {
+                                  _pickedImages.addAll(images);
+                                });
                               }
-                            });
-                          },
-                          maxLength: 10,
-                          keyboardType: TextInputType.number,
-                          decoration: const InputDecoration(
-                              hintText: "₩",
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(color: Color(0xffD9D9D9),),
-                                borderRadius: BorderRadius.all(Radius.circular(10)),
-                              )),
-                        )
-                      ])),
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xffD9D9D9),
+                              fixedSize: Size(70, 70),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Icon(Icons.image, color: Color(0xff757575),),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            "제목",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "필수 입력값입니다.";
+                              }
+                            },
+                            onSaved: (value) {
+                              setState(() {
+                                _titleValue = value!;
+                              });
+                            },
+                            maxLength: 15,
+                            decoration: const InputDecoration(
+                                hintText: "제목",
+                                hintStyle: TextStyle(color: Color(0xffD9D9D9)),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Color(0xffD9D9D9),
+                                    width: 30
+                                  ),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                )),
+                          ),
+                          Text("설명", style: TextStyle(fontSize: 15)),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "필수 입력값입니다.";
+                              }
+                            },
+                            onSaved: (value) {
+                              setState(() {
+                                _descriptionValue = value!;
+                              });
+                            },
+                            maxLength: 200,
+                            maxLines: 6,
+                            decoration: const InputDecoration(
+                                hintText: "어떤 문제가 있는지 자세히 설명해 주세요.",
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Color(0xffD9D9D9),
+                                  ),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                )),
+                          ),
+                          Text("예상 청구 금액 (선택 사항)",
+                              style: TextStyle(fontSize: 15)),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            onSaved: (value) {
+                              setState(() {
+                                if (value != null && value.isNotEmpty) {
+                                  _estimateValue = int.parse(value);
+                                } else {
+                                  _estimateValue = 0;
+                                }
+                              });
+                            },
+                            maxLength: 10,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                                hintText: "₩",
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Color(0xffD9D9D9),
+                                  ),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                )),
+                          )
+                        ])),
+              ),
             )));
   }
 }
