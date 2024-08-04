35215e26fefb2cf71d89eb26a17c11b4c4593e34
KimSihyun <rainy02190@gmail.com>
2024-08-04 15:15:55 +0900

    Feat:소셜 로그인 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:telo/screens/home_page.dart';
 import 'package:telo/screens/notification_page.dart';
 import 'package:telo/screens/repair/repair_list_page.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'login_platform.dart';
 
 void main() {
+  KakaoSdk.init(
+      nativeAppKey: 'aa'
+  );
+
   runApp(const MaterialApp(
-    //home: MyApp(),
-    home: MainPage()
+    home: MyApp(),
+    //home: MainPage()
     //home: AfterLogin(),
   ));
 }
+
+class KakaoLoginApi {
+  signWithKakao() async {
+    if (await isKakaoTalkInstalled()) {
+      try {
+        await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공');
+      } catch (error) {
+        print('카카오톡으로 로그인 실패 $error');
+
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+        try {
+          await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공');
+        } catch (error) {
+          print('카카오계정으로 로그인 실패 $error');
+        }
+      }
+    } else {
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+        print('카카오계정으로 로그인 성공');
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
+    }
+  }
+}
+
+class GoogleLoginApi {
+  LoginPlatform _loginPlatform = LoginPlatform.none;
+
+  void signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    if (googleUser != null) {
+      print('name = ${googleUser.displayName}');
+      print('email = ${googleUser.email}');
+      print('id = ${googleUser.id}');
+
+      // setState(() {
+      //   _loginPlatform = LoginPlatform.google;
+      // });
+    }
+  }
+}
+
+Future<bool> isLoggedIn() async {
+  try {
+    // 유저 정보를 가져오려 시도
+    final User user = await UserApi.instance.me();
+    return user != null;
+  } catch (e) {
+    return false;
+  }
+}
+
+// void signOut() async {
+//   switch (_loginPlatform) {
+//     case LoginPlatform.google:
+//       await GoogleSignIn().signOut();
+//       break;
+//     case LoginPlatform.kakao:
+//       break;
+//     case LoginPlatform.none:
+//       break;
+//   }
+//
+//   setState(() {
+//     _loginPlatform = LoginPlatform.none;
+//   });
+// }
 
 // 로그인 전
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _PageSlideState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _PageSlideState extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FutureBuilder<bool>(
+        future: isLoggedIn(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          } else if (snapshot.hasError) {
+            return Scaffold(
+              body: Center(child: Text('Error occurred')),
+            );
+          } else {
+            bool loggedIn = snapshot.data ?? false;
+            if (loggedIn) {
+              return AfterLogin();
+            } else {
+              return PageSlide();
+            }
+          }
+        },
+      ),
+    );
+  }
+}
+
+class PageSlide extends StatelessWidget {
   final PageController _pageController = PageController(initialPage: 0);
+
+  PageSlide({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -84,39 +198,50 @@ class _PageSlideState extends State<MyApp> {
               ),
             ),
           ),
+          LoginApiButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class LoginApiButton extends StatelessWidget {
+  const LoginApiButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsetsDirectional.only(bottom: 50),
+      alignment: Alignment.bottomCenter,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
           Container(
-            padding: EdgeInsetsDirectional.only(bottom: 50),
-            alignment: Alignment.bottomCenter,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  margin: EdgeInsets.all(7),
-                  child: IconButton(
-                    padding: EdgeInsets.zero,
-                    icon: Image.asset(
-                      'assets/image/android_neutral_sq_SI@1x.png',
-                      width: 200,
-                      fit: BoxFit.contain,
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(7),
-                  child: IconButton(
-                    padding: EdgeInsets.zero,
-                    icon: Image.asset(
-                      'assets/image/kakao_login_medium_narrow.png',
-                      width: 200,
-                      fit: BoxFit.contain,
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
-              ],
+            margin: EdgeInsets.all(7),
+            child: IconButton(
+              padding: EdgeInsets.zero,
+              icon: Image.asset(
+                'assets/image/android_neutral_sq_SI@1x.png',
+                width: 200,
+                fit: BoxFit.contain,
+              ),
+              onPressed: () {},
             ),
-          )
+          ),
+          Container(
+            margin: EdgeInsets.all(7),
+            child: IconButton(
+              padding: EdgeInsets.zero,
+              icon: Image.asset(
+                'assets/image/kakao_login_medium_narrow.png',
+                width: 200,
+                fit: BoxFit.contain,
+              ),
+              onPressed: () {
+                KakaoLoginApi().signWithKakao();
+              },
+            ),
+          ),
         ],
       ),
     );
@@ -154,7 +279,13 @@ class AfterLogin extends StatelessWidget {
                     borderRadius: BorderRadius.circular(7),
                   ),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  //데베에 전송하고 처음 에 role > !=null 일때 다음페이지로? 아니면 isLogged에서 role 이 null 일때만 이페이지로
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainPage()),
+                  );
+                },
                 child: Text(
                   '집주인입니다',
                   style: TextStyle(color: Colors.white),
@@ -171,7 +302,12 @@ class AfterLogin extends StatelessWidget {
                     borderRadius: BorderRadius.circular(7),
                   ),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainPage()),
+                  );
+                },
                 child: Text(
                   '세입자입니다',
                   style: TextStyle(color: Color(0xff2C2C2C)),
