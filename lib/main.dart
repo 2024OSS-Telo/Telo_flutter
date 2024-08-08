d92a4b3433f0ea4db597b1ee99074f6192a232c9
KimSihyun <rainy02190@gmail.com>
2024-08-08 17:05:45 +0900

    Feat: 구글 로그인 api, 페이지 이동

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,12 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:telo/screens/home_page.dart';
 import 'package:telo/screens/notification_page.dart';
 import 'package:telo/screens/repair/repair_list_page.dart';
-import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'const/login_platform.dart';
+import 'package:dio/dio.dart';
 
 void main() {
-  KakaoSdk.init(nativeAppKey: '7175c6f048b50e0a135ab044f8f3155e');
-
-  runApp(const MaterialApp(
-    home: MyApp(),
-    //home: MainPage()
-    //home: AfterLogin(),
-  ));
+  KakaoSdk.init(nativeAppKey: 'aa');
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -28,32 +23,151 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   LoginPlatform _loginPlatform = LoginPlatform.none;
+  bool _isLoading = true;
+  bool _isTenantOrLandlord = false;
+
+  final Dio dio = Dio();
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   @override
   void initState() {
     super.initState();
-    _checkLoginStatus();
+    _initialize();
   }
 
-  void _checkLoginStatus() async {
-    bool isKakaoLoggedIn = await AuthApi.instance.hasToken();
-    bool isGoogleLoggedIn = await GoogleSignIn().isSignedIn();
+  Future<void> _initialize() async {
+    await _checkLoginStatus();
+    await _initializeMemberType();
+  }
 
-    if (isKakaoLoggedIn) {
-      setState(() {
+  Future<void> _checkLoginStatus() async {
+    bool isKakaoLoggedIn = await AuthApi.instance.hasToken();
+    bool isGoogleLoggedIn = await _googleSignIn.isSignedIn();
+
+    setState(() {
+      if (isKakaoLoggedIn) {
         _loginPlatform = LoginPlatform.kakao;
-      });
-    } else if (isGoogleLoggedIn) {
-      setState(() {
+      } else if (isGoogleLoggedIn) {
         _loginPlatform = LoginPlatform.google;
+      } else {
+        _loginPlatform = LoginPlatform.none;
+      }
+    });
+  }
+
+  Future<void> _initializeMemberType() async {
+    print("Initializing member type for platform: $_loginPlatform");
+    if (_loginPlatform != LoginPlatform.none) {
+      await checkMemberType();
+    } else {
+      setState(() {
+        _isLoading = false;
       });
     }
   }
 
+
+  Future<void> signUpUser(String memberID, String memberName, String profile, String provider) async {
+    final response = await dio.post(
+      'http://218.147.229.120:80/api/members/signup',
+      data: {
+        'memberID': memberID,
+        'memberName': memberName,
+        'profile': profile,
+        'provider': provider,
+        'memberType': 'user'
+      },
+      options: Options(
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      ),
+    );
+
+    if (response.statusCode != 200) {
+      print('요청 전송에 실패했습니다.: ${response.statusMessage}');
+    }
+  }
+
+  Future<void> updateUserType(String memberType) async {
+    final String memberID = await findMemberID();
+    print('Member ID: $memberID');
+    print('Member Type: $memberType');
+    print('Member Type Type: ${memberType.runtimeType}');
+
+    final response = await dio.post(
+      'http://218.147.229.120:80/api/members/updateMemberType/$memberID',
+      data: {'memberType': memberType},
+      options: Options(
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      ),
+    );
+
+    if (response.statusCode != 200) {
+      print('Request failed: ${response.statusMessage}');
+    }
+  }
+
+  Future<String> findMemberID() async {
+    if (_loginPlatform == LoginPlatform.kakao) {
+      final user = await UserApi.instance.me();
+      String memberID = user.id.toString();
+      print('MemberID: $memberID');
+      print('Type of MemberID: ${memberID.runtimeType}');
+      return memberID;
+    } else if (_loginPlatform == LoginPlatform.google) {
+      final GoogleSignInAccount? googleUser = _googleSignIn.currentUser;
+      if (googleUser != null) {
+        final String memberID = googleUser.id;
+        print('Google MemberID: $memberID');
+        return memberID;
+      } else {
+        throw Exception('구글 로그인 정보가 없습니다.');
+      }
+    } else {
+      throw Exception('로그인 플랫폼을 찾을 수 없습니다.');
+    }
+  }
+
+  Future<void> checkMemberType() async {
+    try {
+      final String memberID = await findMemberID();
+      print("Checking member type for memberID: $memberID");
+      final response = await dio.get('http://218.147.229.120:80/api/members/$memberID');
+      if (response.statusCode == 200) {
+        final memberType = response.data['memberType'];
+        print("Member Type: $memberType");
+        setState(() {
+          _isTenantOrLandlord =
+              memberType == 'tenant' || memberType == 'landlord';
+          _isLoading = false;
+        });
+      } else {
+        setState(() {
+          _isTenantOrLandlord = false;
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _isTenantOrLandlord = false;
+        _isLoading = false;
+      });
+      print("Error occurred: $e");
+    }
+  }
+
   void signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
 
     if (googleUser != null) {
+      final String memberID = googleUser.id;
+      final String memberName = googleUser.displayName ?? '';
+      final String profile = googleUser.photoUrl ?? '';
+      const String provider = 'google';
+
       print('name = ${googleUser.displayName}');
       print('email = ${googleUser.email}');
       print('id = ${googleUser.id}');
@@ -61,16 +175,24 @@ class _MyAppState extends State<MyApp> {
       setState(() {
         _loginPlatform = LoginPlatform.google;
       });
+
+      await signUpUser(memberID, memberName, profile, provider);
+      await _initializeMemberType();
     }
   }
 
   void signInWithKakao() async {
+    bool isLoggedIn = false;
+    User? user;
+
     if (await isKakaoTalkInstalled()) {
       try {
         await UserApi.instance.loginWithKakaoTalk();
         setState(() {
           _loginPlatform = LoginPlatform.kakao;
         });
+
+        isLoggedIn = true;
         print('카카오톡으로 로그인 성공');
       } catch (error) {
         print('카카오톡으로 로그인 실패 $error');
@@ -83,6 +205,8 @@ class _MyAppState extends State<MyApp> {
           setState(() {
             _loginPlatform = LoginPlatform.kakao;
           });
+
+          isLoggedIn = true;
           print('카카오계정으로 로그인 성공');
         } catch (error) {
           print('카카오계정으로 로그인 실패 $error');
@@ -94,9 +218,29 @@ class _MyAppState extends State<MyApp> {
         setState(() {
           _loginPlatform = LoginPlatform.kakao;
         });
+
+        isLoggedIn = true;
         print('카카오계정으로 로그인 성공');
       } catch (error) {
         print('카카오계정으로 로그인 실패 $error');
+      }
+    }
+
+    if (isLoggedIn) {
+      try {
+        user = await UserApi.instance.me();
+        String memberID = user.id.toString();
+        String memberName = user.kakaoAccount?.profile?.nickname ?? '';
+        String profile = user.kakaoAccount?.profile?.profileImageUrl ?? '';
+        String provider = 'kakao';
+
+        print('로그인 성공 후 user 정보: $user');
+        print( '서버 요청 본문: memberID=$memberID, memberName=$memberName, profile=$profile, provider=$provider');
+
+        await signUpUser(memberID, memberName, profile, provider);
+        await _initializeMemberType();
+      } catch (error) {
+        print('사용자 정보 가져오기 실패: $error');
       }
     }
   }
@@ -115,29 +259,40 @@ class _MyAppState extends State<MyApp> {
 
     setState(() {
       _loginPlatform = LoginPlatform.none;
+      _isLoading = true;
+      _isTenantOrLandlord = false;
     });
+
+    //await _checkLoginStatus();
+    //await _initializeMemberType();
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => MyApp()),
+          (Route<dynamic> route) => false,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_loginPlatform == LoginPlatform.none) {
+    if (_isLoading) {
+      return Center(child: CircularProgressIndicator());
+    } else if (_loginPlatform == LoginPlatform.none) {
       return PageSlide(
         onGoogleSignIn: signInWithGoogle,
         onKakaoSignIn: signInWithKakao,
       );
     } else {
-      bool userInfoExists = false;
-      // bool userInfoExists = await checkUserInfoExists();
-      if (!userInfoExists) {
-        return AfterLogin(onSignOut: signOut);
-      } else {
+      print("_isTenantOrLandlord: $_isTenantOrLandlord");
+      if (_isTenantOrLandlord) {
         return MainPage(onSignOut: signOut);
+      } else {
+        return AfterLogin(
+          onSignOut: signOut,
+          updateService: updateUserType,
+        );
       }
     }
-  }
-
-  Future<bool> checkUserInfoExists() async {
-    return true;
   }
 }
 
@@ -251,8 +406,9 @@ class PageSlide extends StatelessWidget {
 
 class AfterLogin extends StatelessWidget {
   final VoidCallback onSignOut;
+  final Future<void> Function(String) updateService;
 
-  const AfterLogin({super.key, required this.onSignOut});
+  const AfterLogin({super.key, required this.onSignOut, required this.updateService});
 
   @override
   Widget build(BuildContext context) {
@@ -282,11 +438,13 @@ class AfterLogin extends StatelessWidget {
                     borderRadius: BorderRadius.circular(7),
                   ),
                 ),
-                onPressed: () {
-                  //데베에 전송하고 처음 에 role > !=null 일때 다음페이지로? 아니면 isLogged에서 role 이 null 일때만 이페이지로
+                onPressed: () async {
+                  await updateService('landlord');
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => MainPage(onSignOut: onSignOut)),
+                    MaterialPageRoute(
+                      builder: (context) => MainPage(onSignOut: onSignOut),
+                    ),
                   );
                 },
                 child: Text(
@@ -305,10 +463,13 @@ class AfterLogin extends StatelessWidget {
                     borderRadius: BorderRadius.circular(7),
                   ),
                 ),
-                onPressed: () {
+                onPressed: () async {
+                  await updateService('tenant');
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => MainPage(onSignOut: onSignOut)),
+                    MaterialPageRoute(
+                      builder: (context) => MainPage(onSignOut: onSignOut),
+                    ),
                   );
                 },
                 child: Text(
@@ -327,7 +488,7 @@ class AfterLogin extends StatelessWidget {
 class MainPage extends StatefulWidget {
   final VoidCallback onSignOut;
 
-  const MainPage({Key? key, required this.onSignOut}) : super(key: key);
+  const MainPage({super.key, required this.onSignOut});
 
   @override
   State<MainPage> createState() => _MainPageState();
@@ -353,6 +514,11 @@ class _MainPageState extends State<MainPage> {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  Future<bool> _onWillPop() async {
+    SystemNavigator.pop();
+    return Future.value(false);
   }
 
   @override
